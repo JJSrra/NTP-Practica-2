eb8d941c92dbe7db464f8d106b994c83baa1fd25
Juanjo Sierra <jjsierra103@gmail.com>
2018-04-18 11:11:05 +0200

    Añadido test unitario para comprobar elementos internos del triángulo de Pascal

--- src/test/scala/FuncionesTest.scala
+++ src/test/scala/FuncionesTest.scala
@@ -3,8 +3,9 @@ import org.scalacheck.Prop.{forAll}
 
 object FuncionesTest extends Properties("FuncionesTest"){
 
+  val MAXIMO = 20
   val coordenadasExtremos = for {
-    fila <- Gen.choose(0,20)
+    fila <- Gen.choose(0,MAXIMO)
     columna <- Gen.oneOf(0, fila)
   } yield (fila, columna)
 
@@ -12,6 +13,19 @@ object FuncionesTest extends Properties("FuncionesTest"){
     forAll(coordenadasExtremos) { (i) => {
       val resultado = Funciones.calcularValorTrianguloPascal(i._1, i._2)
       resultado == 1
+    }}
+  }
+
+  val coordenadasInternas = for {
+    fila <- Gen.choose(2,MAXIMO)
+    columna <- Gen.choose(1,fila-1)
+  } yield (fila, columna)
+
+  property("Elementos internos del triángulo correctos") = {
+    forAll(coordenadasInternas) { (i) => {
+      val resultado = Funciones.calcularValorTrianguloPascal(i._1, i._2)
+      resultado == Funciones.calcularValorTrianguloPascal(i._1 -1, i._2 -1) +
+                  Funciones.calcularValorTrianguloPascal(i._1 -1, i._2)
     }}
   }
 }
