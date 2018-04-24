51dced365072786a6d3f33613e502d449d2c2d7c
Juanjo Sierra <jjsierra103@gmail.com>
2018-04-24 10:12:51 +0200

    Añadido test de Scalacheck para método de chequeo de paréntesis

--- src/test/scala/FuncionesTest.scala
+++ src/test/scala/FuncionesTest.scala
@@ -3,6 +3,10 @@ import org.scalacheck.Prop.{forAll}
 
 object FuncionesTest extends Properties("FuncionesTest"){
 
+  // =============================================================
+
+  // TRIÁNGULO DE PASCAL
+
   val MAXIMO = 20
   val coordenadasExtremos = for {
     fila <- Gen.choose(0,MAXIMO)
@@ -26,6 +30,33 @@ object FuncionesTest extends Properties("FuncionesTest"){
       val resultado = Funciones.calcularValorTrianguloPascal(i._1, i._2)
       resultado == Funciones.calcularValorTrianguloPascal(i._1 -1, i._2 -1) +
                   Funciones.calcularValorTrianguloPascal(i._1 -1, i._2)
+    }}
+  }
+
+  // =============================================================
+
+  // CHEQUEAR PARÉNTESIS
+
+  val strGen = (n: Int) =>
+    Gen.listOfN(n, Gen.oneOf('(',')',Gen.alphaChar.sample.get))
+
+  def comprobarParentesis(lista: List[Char]): Boolean = {
+
+    for (i <- 1 to lista.length){
+      val sublista = lista.slice(0,i)
+      if (sublista.count("("==) < sublista.count(")"==)) return false
+    }
+    return true
+  }
+
+  property("El resultado del balance de paréntesis es correcto") = {
+    forAll(strGen(10)) { (cadena) => {
+
+      // En este caso la propiedad de que las subcadenas tengan al menos el mismo número de paréntesis
+      // de apertura que de cierre sirve para confirmar que la cadena es incorrecta, pero no para
+      // detectar si la cadena está bien formada. Por tanto, en la propiedad interpreto que los resultados
+      // de ambos métodos sea false o que el de la propiedad sea true.
+      (!Funciones.chequearBalance(cadena) == !comprobarParentesis(cadena)) || comprobarParentesis(cadena)
     }}
   }
 }
